d7ddeb812fe22641b754c83ae7177ea89a23405d
qhnu <73204221+qhnu@users.noreply.github.com>
2021-02-17 08:03:55 +0900

    build: update dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import React, { useEffect } from 'react'
+import { RecoilRoot } from 'recoil'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '../components/head'
 import { defaultTheme } from '../components/head/GlobalStyle/theme'
@@ -11,12 +12,12 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
     document.documentElement.setAttribute('lang', navigator.language)
   }, [])
   return (
-    <>
+    <RecoilRoot>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </RecoilRoot>
   )
 }
 
